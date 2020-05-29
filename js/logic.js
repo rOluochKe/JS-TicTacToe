a454d48fae55428fc60aede0a326856070754c92
leonmezu1 <leo.mez@outlook.com>
2020-05-29 13:03:44 -0500

    Fixes required by TSE are now implemented

--- js/logic.js
+++ js/logic.js
@@ -1,60 +1,48 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-use-before-define */
 
-// let players = [];
-const players = getPlayersLS();
-// turn
-let xTurn = true;
-// Winning combinations
-const winCombinations = [[0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]];
-
-
-// Player objects
 const Player = (name, wins) => {
   const pName = name;
   const pWins = wins;
   return { pName, pWins };
 };
 
-// Check for winnings
-function winCheck() {
-  const playerWinner1 = winCombinations.some(combination => combination.every(index => cellsArr[index].dataset.player === '1'));
-  const playerWinner2 = winCombinations.some(combination => combination.every(index => cellsArr[index].dataset.player === '2'));
-  return [playerWinner1, playerWinner2];
-}
+const Game = () => {
+  const winCombinations = [
+    [0, 1, 2], [3, 4, 5],
+    [6, 7, 8], [0, 3, 6],
+    [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]];
 
-// Reset play
-function resetGame() {
+  const getPlayersLS = () => {
+    const playersArr = localStorage.getItem('players') === null ? [] : JSON.parse(localStorage.getItem('players'));
+    return playersArr;
+  };
+
+  const players = getPlayersLS();
+
+  let xTurn = false;
+
+  const turnSwap = () => {
+    xTurn = !xTurn;
+    return xTurn;
+  };
+
+  const winCheck = () => {
+    const playerWinner1 = winCombinations.some(combination => combination.every(index => cellsArr[index].dataset.player === '1'));
+    const playerWinner2 = winCombinations.some(combination => combination.every(index => cellsArr[index].dataset.player === '2'));
+    return [playerWinner1, playerWinner2];
+  };
+
+  const updatePlayerStats = (playersToUpdate) => {
+    localStorage.setItem('players', JSON.stringify(playersToUpdate));
+  };
+
+  return {
+    players, updatePlayerStats, turnSwap, winCheck,
+  };
+};
+
+const resetGame = () => {
   window.location.reload();
-}
-
-// Turn player
-function turnSwap() {
-  xTurn = !xTurn;
-}
-
-// Updating localStorage
-function getPlayersLS() {
-  let playersArr;
-
-  if (localStorage.getItem('players') === null) {
-    playersArr = [];
-  } else {
-    playersArr = JSON.parse(localStorage.getItem('players'));
-  }
-  return playersArr;
-}
-
-// Updating players statistics
-function updatePlayerStats(players) {
-  localStorage.setItem('players', JSON.stringify([]));
-  localStorage.setItem('players', JSON.stringify(players));
-}
+};
